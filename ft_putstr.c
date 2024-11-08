7ed73a65f3284020015ea30606fa8a7fe8cc76e0
Celtenn <enescelten@gmail.com>
2024-11-08 10:50:03 +0300

    Add files via upload

--- ft_putstr.c
+++ ft_putstr.c
@@ -2,8 +2,16 @@
 
 void ft_putstr(char *str, int *len_count)
 {
+    char *strnull = "(null)";
     if (!str)
-        return ;
+    {
+        while (*strnull)
+        {
+            ft_putchar(*strnull, len_count);
+            strnull++;
+        }
+        return;
+    }
     while (*str)
     {
         ft_putchar(*str, len_count);
